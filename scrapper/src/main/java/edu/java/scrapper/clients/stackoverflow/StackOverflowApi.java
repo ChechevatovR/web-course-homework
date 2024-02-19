6a40d556f0856f110085b3582bbb4e229830b754
Roman Chechevatov <chechevatov.r@gmail.com>
2024-02-19 20:04:26 +0300

    Удалил неиспользуемые методы API Stackoverflow

--- scrapper/src/main/java/edu/java/scrapper/clients/stackoverflow/StackOverflowApi.java
+++ scrapper/src/main/java/edu/java/scrapper/clients/stackoverflow/StackOverflowApi.java
@@ -1,7 +1,5 @@
 package edu.java.scrapper.clients.stackoverflow;
 
-import edu.java.scrapper.clients.stackoverflow.model.api.AnswerResponse;
-import edu.java.scrapper.clients.stackoverflow.model.api.CommentResponse;
 import edu.java.scrapper.clients.stackoverflow.model.api.QuestionResponse;
 import edu.java.scrapper.clients.stackoverflow.model.api.Wrapper;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +8,4 @@ import org.springframework.web.service.annotation.GetExchange;
 public interface StackOverflowApi {
     @GetExchange("/questions/{id}")
     Wrapper<QuestionResponse> getQuestion(@PathVariable int id);
-
-    @GetExchange("/questions/{id}/answers")
-    Wrapper<AnswerResponse> getAnswersForQuestion(@PathVariable int id);
-
-    @GetExchange("/posts/{id}/comments")
-    Wrapper<CommentResponse> getCommentsForPost(@PathVariable int id);
 }
--- scrapper/src/main/java/edu/java/scrapper/clients/stackoverflow/StackOverflowClient.java
+++ scrapper/src/main/java/edu/java/scrapper/clients/stackoverflow/StackOverflowClient.java
@@ -1,11 +1,6 @@
 package edu.java.scrapper.clients.stackoverflow;
 
-import edu.java.scrapper.clients.stackoverflow.model.Answer;
-import edu.java.scrapper.clients.stackoverflow.model.Comment;
 import edu.java.scrapper.clients.stackoverflow.model.Question;
-import edu.java.scrapper.clients.stackoverflow.model.api.AnswerResponse;
-import edu.java.scrapper.clients.stackoverflow.model.api.CommentResponse;
-import java.util.List;
 
 public class StackOverflowClient {
     protected final StackOverflowApi api;
@@ -16,13 +11,5 @@ public class StackOverflowClient {
 
     public Question getQuestion(int id) {
         return api.getQuestion(id).items().get(0).asModel();
-    }
-
-    public List<Answer> getAnswersForQuestion(int id) {
-        return api.getAnswersForQuestion(id).items().stream().map(AnswerResponse::asModel).toList();
-    }
-
-    public List<Comment> getCommentsForPost(int id) {
-        return api.getCommentsForPost(id).items().stream().map(CommentResponse::asModel).toList();
     }
 }
