1ae800724af3688085d4462f54e8dd5f3edad1f3
Roman Chechevatov <chechevatov.r@gmail.com>
2024-02-27 16:02:20 +0300

    Обработчики ошибок в контроллерах

---
+++ scrapper/src/main/java/edu/java/scrapper/controllers/RestExceptionHandler.java
@@ -0,0 +1,65 @@
+package edu.java.scrapper.controllers;
+
+import edu.java.scrapper.controllers.model.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<ApiErrorResponse> handleException(Exception e) {
+        ApiErrorResponse response = new ApiErrorResponse();
+
+        if (e instanceof ErrorResponse errorResponse) {
+            fillResponseFromErrorResponse(response, errorResponse);
+        }
+        fillResponseFromException(response, e);
+
+        HttpStatus responseCode;
+        try {
+            responseCode = HttpStatus.valueOf(Integer.parseInt(response.getCode()));
+        } catch (IllegalArgumentException exception) {
+            responseCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<>(response, responseCode);
+    }
+
+    public static void fillResponseFromErrorResponse(ApiErrorResponse response, ErrorResponse e) {
+        if (response.getCode() == null) {
+            response.setCode(Integer.toString(e.getBody().getStatus()));
+        }
+        if (response.getDescription() == null) {
+            response.setDescription(e.getBody().getDetail());
+        }
+    }
+
+    public static void fillResponseFromException(ApiErrorResponse response, Exception e) {
+        if (response.getCode() == null) {
+            if (e instanceof HttpMessageNotReadableException) {
+                response.setCode("400");
+            } else {
+                response.setCode("500");
+            }
+        }
+        if (response.getExceptionName() == null) {
+            response.setExceptionName(e.getClass().getSimpleName());
+        }
+        if (response.getExceptionMessage() == null) {
+            response.setExceptionMessage(e.getMessage());
+        }
+        if (response.getDescription() == null) {
+            response.setDescription(e.toString());
+        }
+        if (response.getStacktrace() == null) {
+            response.setStacktrace(
+                Arrays.stream(e.getStackTrace()).map(StackTraceElement::toString).toList()
+            );
+        }
+    }
+}
---
+++ scrapper/src/test/java/edu/java/scrapper/RestExceptionHandlerTest.java
@@ -0,0 +1,90 @@
+package edu.java.scrapper;
+
+import edu.java.scrapper.controllers.model.ApiErrorResponse;
+import java.net.URI;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RestExceptionHandlerTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void testUnknownEndpoint() {
+        ResponseEntity<ApiErrorResponse> response =
+            restTemplate.getForEntity(URI.create("http://localhost:" + port + "/"), ApiErrorResponse.class);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.NOT_FOUND.value(), Integer.valueOf(response.getBody().getCode()));
+    }
+
+    @Test
+    void testMethodNotSupported() {
+        ResponseEntity<ApiErrorResponse> response =
+            restTemplate.getForEntity(URI.create("http://localhost:" + port + "/tg-chat/123"), ApiErrorResponse.class);
+
+        Assertions.assertEquals(HttpStatus.METHOD_NOT_ALLOWED, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.METHOD_NOT_ALLOWED.value(), Integer.valueOf(response.getBody().getCode()));
+    }
+
+    @Test
+    void testNoPayload() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>("", headers);
+
+        ResponseEntity<ApiErrorResponse> response = restTemplate.exchange(
+            URI.create("http://localhost:" + port + "/links"),
+            HttpMethod.POST,
+            entity,
+            ApiErrorResponse.class
+        );
+
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertEquals(
+            HttpStatus.BAD_REQUEST.value(),
+            Integer.valueOf(response.getBody().getCode())
+        );
+        Assertions.assertTrue(response.getBody().getDescription().contains("Required header"));
+    }
+
+    @Test
+    void testInvalidPayload() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Tg-Chat-Id", "123");
+        HttpEntity<String> entity = new HttpEntity<>(
+            """
+            {
+            }
+            """,
+            headers
+        );
+
+        ResponseEntity<ApiErrorResponse> response = restTemplate.exchange(
+            URI.create("http://localhost:" + port + "/links"),
+            HttpMethod.POST,
+            entity,
+            ApiErrorResponse.class
+        );
+
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), Integer.valueOf(response.getBody().getCode()));
+        Assertions.assertTrue(response.getBody().getDescription().contains("Invalid request content"));
+    }
+}
