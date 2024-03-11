20fa084ded24a29499a8542bd6868ec3a4f34a59
Roman Chechevatov <chechevatov.r@gmail.com>
2024-03-11 17:00:54 +0300

    Увеличил размер telegram_chat_id в таблице chats

--- migrations/sql/01-chats.sql
+++ migrations/sql/01-chats.sql
@@ -3,5 +3,5 @@ create table public.chats
     id               serial
         constraint chats_pk
             primary key,
-    telegram_chat_id integer not null
+    telegram_chat_id bigint not null
 );
